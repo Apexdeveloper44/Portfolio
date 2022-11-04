fa4e5e516f1c3aaf4fca7c7a7393ee6169825761
Divyesh Kumar <dkcdgr8@gmail.com>
2022-11-04 13:21:34 +0530

    first commit

---
+++ src/components/portfoliolist/PortfolioList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./portfoliolist.scss";
+
+export default function PortfolioList({id,title ,active , setSelected}) {
+  return (
+    <li className={active ? "portfolioList active" : "portfolioList"} 
+    onClick={() => setSelected(id)}>
+      {title}
+    </li>
+  )
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./topbar.scss"
+import {Person,Mail} from "@material-ui/icons"
+export default function Topbar({menuOpen,setMenuOpen}) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+            <a href="#intro" className="logo">Portfolio.</a>
+            <div className="itemContainer">
+                <Person className='icon' />
+                <span>9958388609</span>
+            </div>
+            <div className="itemContainer">
+                <Mail className='icon' />
+                <span>dkcdgr8@gmail.com</span>
+            </div>
+        </div>
+        <div className= "right">
+            <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+            </div>
+        </div>
+     </div>
+    </div>
+  )
+}
